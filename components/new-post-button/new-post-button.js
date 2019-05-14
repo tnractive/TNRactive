5b41a1a9d04c2122f59f4d202f09f5b9e116d8a0
9968 <yannisxbolliger@gmail.com>
2019-05-14 09:41:43 +0200

    created new-post-button component

---
+++ components/new-post-button/new-post-button.js
@@ -0,0 +1,23 @@
+// components/new-post-button/new-post-button.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
